dac305354400fca67aeb17cd93cac9f43334e40d
Drew J. Sonne <drew.sonne@gmail.com>
2018-04-28 08:31:36 +0100

    Add test cases

--- gocd/role_gocd_test.go
+++ gocd/role_gocd_test.go
@@ -10,13 +10,13 @@ import (
 )
 
 func TestRole(t *testing.T) {
-	setup()
-	defer teardown()
-
-	t.Run("GoCD", testRoleGoCD)
+	t.Run("Create/GoCD", testRoleCreateGoCD)
+	t.Run("List", testRoleList)
 }
 
-func testRoleGoCD(t *testing.T) {
+func testRoleCreateGoCD(t *testing.T) {
+	setup()
+	defer teardown()
 	mux.HandleFunc("/api/admin/security/roles", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, r.Method, "POST", "Unexpected HTTP method")
 		assert.Equal(t, apiV1, r.Header.Get("Accept"))
@@ -46,4 +46,48 @@ func testRoleGoCD(t *testing.T) {
 		},
 	}, r)
 
+}
+
+func testRoleList(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc("/api/admin/security/roles", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Unexpected HTTP method")
+		assert.Equal(t, apiV1, r.Header.Get("Accept"))
+
+		j, _ := ioutil.ReadFile("test/resources/role.2.json")
+
+		fmt.Fprint(w, string(j))
+	})
+
+	r, _, err := client.Roles.List(context.Background(), nil)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, []*Role{
+		{
+			Name: "spacetiger",
+			Type: "gocd",
+			Attributes: &RoleAttributesGoCD{
+				Users: []string{"alice", "bob", "robin"},
+			},
+		},
+		{
+			Name: "blackbird",
+			Type: "plugin",
+			Attributes: &RoleAttributesGoCD{
+				AuthConfigId: "ldap",
+				Properties: []*PluginConfigurationKVPair{
+					{
+						Key:   "UserGroupMembershipAttribute",
+						Value: "memberOf",
+					},
+					{
+						Key:   "GroupIdentifiers",
+						Value: "ou=admins,ou=groups,ou=system,dc=example,dc=com",
+					},
+				},
+			},
+		},
+	}, r)
 }
