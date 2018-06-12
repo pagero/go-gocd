718d2730ad89b1dd3d03cecc6e09d1438867c241
Drew J. Sonne <drew.sonne@beamly.com>
2018-06-12 15:44:13 +0100

    Added test cases for server version resource

--- gocd/resource_server_version_test.go
+++ gocd/resource_server_version_test.go
@@ -2,12 +2,16 @@ package gocd
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
+	"net/http"
 )
 
 func testServerVersionResource(t *testing.T) {
 	t.Run("LessThan", testServerVersionLessThan)
 	t.Run("Equal", testServerVersionEqual)
+	t.Run("GetApiVersion", testServerVersionGetApiVersion)
+	t.Run("GetApiVersionFail", testServerVersionGetApiVersionFail)
 }
 
 func testServerVersionEqual(t *testing.T) {
@@ -31,7 +35,8 @@ func testServerVersionLessThan(t *testing.T) {
 		want bool
 	}{
 		{v1: &ServerVersion{Version: "1.0.0"}, v2: &ServerVersion{Version: "2.0.0"}, want: true},
-		{v1: &ServerVersion{Version: "2.0.1"}, v2: &ServerVersion{Version: "2.0.0"}, want: true},
+		{v1: &ServerVersion{Version: "2.0.1"}, v2: &ServerVersion{Version: "2.0.0"}, want: false},
+		{v1: &ServerVersion{Version: "2.0.0"}, v2: &ServerVersion{Version: "2.0.1"}, want: true},
 		{v1: &ServerVersion{Version: "2.0.0"}, v2: &ServerVersion{Version: "1.0.0"}, want: false},
 	} {
 		test.v1.parseVersion()
@@ -39,5 +44,45 @@ func testServerVersionLessThan(t *testing.T) {
 
 		assert.Equal(t, test.want, test.v1.LessThan(test.v2))
 		assert.Equal(t, !test.want, test.v2.LessThan(test.v1))
+	}
+}
+
+func testServerVersionGetApiVersion(t *testing.T) {
+	for _, test := range []struct {
+		v        *ServerVersion
+		endpoint string
+		method   string
+		want     string
+	}{
+		{
+			endpoint: "/api/version",
+			method:   http.MethodGet,
+			want:     apiV1,
+		},
+	} {
+		apiV, err := test.v.GetApiVersion(test.endpoint, test.method)
+
+		assert.NoError(t, err)
+		assert.Equal(t, apiV, test.want)
+	}
+}
+
+func testServerVersionGetApiVersionFail(t *testing.T) {
+	for _, test := range []struct {
+		v        *ServerVersion
+		endpoint string
+		method   string
+		want     string
+	}{
+		{
+			endpoint: "/api/foobar",
+			method:   http.MethodGet,
+			want:     apiV1,
+		},
+	} {
+		apiV, err := test.v.GetApiVersion(test.endpoint, test.method)
+
+		assert.EqualError(t, err, test.want)
+		assert.Empty(t, apiV)
 	}
 }
